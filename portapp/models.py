84c90957234080968723ba451dc6cbc8189ef23b
Ferdous ahmed <helloferdous@gmail.com>
2020-10-10 15:26:19 +0600

    fixed issues with portapp model

--- portapp/models.py
+++ portapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Port(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100,blank=True)
     image = models.ImageField(upload_to='media/',blank=True)
     description = models.TextField(blank=True)
     
     url =models.URLField(blank=True)
+    port_title =models.CharField(max_length=100,blank=True)
     
     def __str__(self): 
         return self.title
